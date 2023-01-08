b29d93d4a9758777c1918f4a0c4254a9d49f364c
Priestly Patrick bassey <pbassey30@gmail.com>
2023-01-08 22:59:12 +0100

    added a graphic work for Habeebah

--- src/components/qualifications/qualifications.jsx
+++ src/components/qualifications/qualifications.jsx
@@ -16,6 +16,7 @@ const Qualifications = () => {
                 <div class="child"><img class="child-img" src="https://i.pinimg.com/564x/d3/21/e4/d321e4e4739d77eedf91f25db59a3e73.jpg" alt="graphic-pic" /></div>
                 <div class="child"><img class="child-img" src="https://i.pinimg.com/564x/30/54/bb/3054bb2909d87753abf5bf24f4e3f124.jpg" alt="graphic-pic" /></div>
                 <div class="child"><img class="child-img" src="https://i.pinimg.com/564x/bf/16/b4/bf16b47521de01f3d45ae5462bf2c362.jpg" alt="graphic-pic" /></div>
+                <div class="child"><img class="child-img" src="https://i.pinimg.com/564x/a0/78/9d/a0789dea443d40636f15db0a39af2f1e.jpg" alt="graphic-pic" /></div>
             </div>
         </div>
     </div>
